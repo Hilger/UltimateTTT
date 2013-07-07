33b63d91f320bd543235c1d05f1d860803cc4ee8
Jeffrey Hilger <hilger328@gmail.com>
2013-07-06 21:20:32 -0700

    Some modifications to the runGame() function

--- UTTTCommand.py
+++ UTTTCommand.py
@@ -1,28 +1,29 @@
-class UTTTMenu:
+class Menu:
 	def __init__(self):
 		pass
 
 	def runMenu(self):
 		user_input = raw_input().split()
 		command = user_input[0]
-		args = user_input[1:]
-		
+
 		if command in commands:
-			self.commands[command](*args)
+			self.commands[command]()
 		else:
 			print "Command not recognized"
+
 		self.runMenu()
 
-	def exit(self, *args):
+	def exit(self):
 		pass
 
-	def newGame(self, *args):
-		runGame()
+	def newGame(self):
+		game = Game()
+		game.runGame()
 
-	def newGamePlus(self, *args):
+	def newGamePlus(self):
 		pass
 
-	def info(self, *args):
+	def info(self):
 		pass
 
 	commands = {
--- UTTTGame.py
+++ UTTTGame.py
@@ -1,22 +1,47 @@
 class Game:
 	def __init__(self):
-		multiBoard = MultiBoard()
-		wins = [None for i in range(0,9)]
-		player1 = "X"
-		player2 = "O"
+		self.multiBoard = MultiBoard()
+		self.player1wins = 0
+		self.player2wins = 0
+		self.currentPlayer = 1
+		self.board = None
+		self.playerIcons = {1: "X", 2 : "O"}
 
-	def detectWin(self, board, player):
+	def detectBoardWin(self, player, board):
 		rows = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8],
 				[0,4,8], [2,4,6]]
 		for row in rows:
 			for space in rows:
-				if board[space] != player:
+				if self.board[space] != self.playerIcons(self.currentPlayer):
 					break
 			else:
 				return True
 		return False
 
-	def makeMove(self, player, board, space):
-		if multiBoard[]
+	def checkFilled(self, board):
+		if any(self.board == "-"):
+			return False
+		return True
 
+	def makeMove(self, player, board, space):
+		board = multiBoard.getBoard(board)
+		if board.getSpace(space) != None:
+			print "That space has already been taken!"
+		else:
+			board.changeSquare(player, square)
+
+	def runGame():
+		# List options for the player
+		# Ask the move they would like to make
+		# Make the move
+		# Return representation of board to console
+		# If one player wins, print and return to menu
+		self.listOptions()
+		print "Player %s please choose a board" % currentPlayer
+		boardChoice = raw_input()
+		if boardChoice not in map(str, range(1,10)):
+			print "That is not an available board.  Choices are 1 to 9"
+			self.runGame()
+		else:
+			self.board = multiBoard.getBoard(boardChoice)
 
