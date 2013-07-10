269e8c95b7d1cbfd191c79310b8bcb695af1d38f
Jeffrey Hilger <hilger328@gmail.com>
2013-07-09 23:46:16 -0700

    Removed unmerged miles

--- UltimateTTT.py
+++
@@ -1,9 +0,0 @@
-from UTTTMenu import Menu
-
-if __name__ == "__main__":
-	# Print out the title card
-	# List the possible commands
-	# Accept command input
-	# Run command
-	menu = Menu()
-	menu.runMenu()
